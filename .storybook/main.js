23a11150f7c6e7df5f7be06084d0336ee38eb995
Alejandro Medina <5almero5@gmail.com>
2020-08-20 13:33:39 -0500

    Add controls to Stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,5 +4,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
+    '@storybook/addon-controls',
+    '@storybook/addon-viewport',
   ],
 };

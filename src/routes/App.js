8fe14b071afa40b66d88328504e7f0ee96a58b37
Alejandro Medina <5almero5@gmail.com>
2020-08-20 13:47:59 -0500

    Add react rotues & remove unless components

---
+++ src/routes/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Switch, HashRouter } from 'react-router-dom';
+
+const App = () => (
+  <HashRouter basename='/'>
+    <Switch>
+      <Route exact path='/' component={() => <h1>HOME</h1>} />
+      <Route
+        component={() => (
+          <h1>
+            404 <br /> Woops{' '}
+          </h1>
+        )}
+      />
+    </Switch>
+  </HashRouter>
+);
+
+export default App;

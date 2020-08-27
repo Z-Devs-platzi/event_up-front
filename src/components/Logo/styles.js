fb0b4c01fa3379caa1295f2469cd0ec8db8d4b63
Alejandro Medina <5almero5@gmail.com>
2020-08-27 06:05:42 -0500

    Fix logo container

--- src/components/Logo/styles.js
+++ src/components/Logo/styles.js
@@ -3,10 +3,11 @@ import defaultBrand from '../../assets/event_up_logo.svg';
 
 export const LogoContainer = styled.div.attrs((props) => ({
   img: props.img ? props.img : defaultBrand,
-  size: props.size ? props.size : '50',
+  size: props.size ? props.size : 50,
 }))`
   background-image: url(${(props) => props.img});
   background-repeat: no-repeat;
   background-size: contain;
   height: ${(props) => `${props.size}px`};
+  width: 250px;
 `;

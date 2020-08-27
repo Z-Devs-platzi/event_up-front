a398ca35e967298eae08f1bf279db2b37f75b714
Alejandro Medina <5almero5@gmail.com>
2020-08-27 05:06:55 -0500

    Add Logo Component

---
+++ src/components/Logo/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import defaultBrand from '../../assets/event_up_logo.svg';
+
+export const LogoContainer = styled.div.attrs((props) => ({
+  img: props.img ? props.img : defaultBrand,
+  size: props.size ? props.size : '50',
+}))`
+  background-image: url(${(props) => props.img});
+  background-repeat: no-repeat;
+  background-size: contain;
+  height: ${(props) => `${props.size}px`};
+`;

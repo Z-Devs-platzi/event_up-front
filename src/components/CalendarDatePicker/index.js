1daafd89687b2b1ae7b1f23328963cbc8b533004
Jorge Ossa <jorgehossa@gmail.com>
2020-08-27 23:24:34 -0500

    Add label and custom styles to input element
    This add the label and set custom input styles to date picker.
    Also load the component empty if not exist an event date loaded from DB

--- src/components/CalendarDatePicker/index.js
+++ src/components/CalendarDatePicker/index.js
@@ -1,27 +1,43 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
+import { LabelText, DateInput } from './styles';
 import PropTypes from 'prop-types';
 
 // import { LabelText } from './InputDateTime.styles';
 import 'react-datepicker/dist/react-datepicker.css';
 
-const InputDateTime = ({ eventDate }) => {
-  const [startDate, setStartDate] = useState(eventDate);
+const CustomField = ({ value, onClick }) => (
+  <DateInput value={value} onClick={onClick} />
+);
+
+const InputDateTime = ({ eventDate, id }) => {
+  const [startDate, setStartDate] = useState(eventDate && null);
   return (
-    <DatePicker
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      showTimeSelect
-      timeFormat='HH:mm'
-      timeIntervals={30}
-      timeCaption='time'
-      dateFormat='MMM dd, yyyy / HH:mm'
-    />
+    <>
+      <LabelText htmlFor={id}>Date / Time</LabelText>
+      <br />
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        showTimeSelect
+        timeFormat='HH:mm'
+        timeIntervals={30}
+        timeCaption='time'
+        dateFormat='MMM dd, yyyy / HH:mm'
+        customInput={<CustomField />}
+      />
+    </>
   );
+};
+
+CustomField.propTypes = {
+  value: PropTypes.any,
+  onClick: PropTypes.func,
 };
 
 InputDateTime.propTypes = {
   eventDate: PropTypes.string,
+  id: PropTypes.string.isRequired,
 };
 
 InputDateTime.defaultProps = {

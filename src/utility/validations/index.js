8b5abd7287868ef75668f23b8cbc903589415b85
Alejandro Medina <5almero5@gmail.com>
2020-08-28 00:49:49 -0500

    Add Validation Match

--- src/utility/validations/index.js
+++ src/utility/validations/index.js
@@ -1,18 +1,40 @@
+import {
+  ERROR_REQUIRED_EMAIL,
+  ERROR_VALID_EMAIL,
+  ERROR_REQUIRED_PW,
+  ERROR_VALID_PW,
+  ERROR_MATCH,
+} from '../consts';
 export const emailValidation = (values) => {
   let error = null;
   if (!values) {
-    error = 'Email address is required';
+    error = ERROR_REQUIRED_EMAIL;
   } else if (!/\S+@\S+\.\S+/.test(values)) {
-    error = 'Email address is invalid';
+    error = ERROR_VALID_EMAIL;
   }
   return error;
 };
 export const passwordValidation = (values) => {
   let error = null;
   if (!values) {
-    error = 'Password required';
+    error = ERROR_REQUIRED_PW;
   } else if (values.length < 6) {
-    error = 'Password need more caracters';
+    error = ERROR_VALID_PW;
+  }
+  return error;
+};
+/**
+ *
+ * @param {String} match the string to be matched
+ *  const matchMesage = matchValidation('This si my passwod');
+ * @return {Function}
+ * @param {String} value the value to mathc
+ *  matchMesage(pw)
+ */
+export const matchValidation = (match) => (value) => {
+  let error = null;
+  if (value !== match) {
+    error = ERROR_MATCH;
   }
   return error;
 };

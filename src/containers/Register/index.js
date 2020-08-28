3c0d588a66b2d1d533a3f409835b0cd17330089a
Alejandro Medina <5almero5@gmail.com>
2020-08-28 00:08:44 -0500

    Add Register page

---
+++ src/containers/Register/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { CenterDiv, CardDiv } from '../../styles/GlobalStyles';
+import { useAuthState } from '../../context/auth-context';
+import { useFormTextField } from '../../utility/hooks/useFormFields';
+import TextInput from '../../components/TextInput';
+import Button from '../../components/Button';
+import Spiner from '../../components/Spiner';
+import LandingWraper from '../../components/LandingWraper';
+import { emailValidation, passwordValidation } from '../../utility/validations';
+const Register = () => {
+  let { register, data } = useAuthState();
+
+  const emailField = useFormTextField('', emailValidation);
+  const passwordField = useFormTextField('', passwordValidation);
+  const passwordFieldRepeat = useFormTextField();
+  const orgField = useFormTextField();
+  const nameField = useFormTextField();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    register();
+  };
+
+  return (
+    <>
+      <LandingWraper>
+        <form onSubmit={handleSubmit}>
+          <CardDiv>
+            <CenterDiv>
+              <TextInput
+                label='Name'
+                logical={nameField}
+                type={'text'}
+                id={'SU_name'}
+              />
+              <TextInput
+                errorMessage={emailField.error}
+                label='Email'
+                logical={emailField}
+                type={'email'}
+                id={'SU_email'}
+              />
+              <TextInput
+                label='Organization'
+                logical={orgField}
+                type={'text'}
+                id={'SU_orgs'}
+              />
+              <TextInput
+                label='Password'
+                errorMessage={passwordField.error}
+                logical={passwordField}
+                type={'password'}
+                id={'SU_password'}
+              />
+              <TextInput
+                label='Repeat password'
+                logical={passwordFieldRepeat}
+                type={'password'}
+                id={'SU_password_Repeat'}
+              />
+
+              <Button type='submit'>
+                {data.status === 'pending' ? <Spiner /> : 'Sing Up'}
+              </Button>
+              <span>
+                Already have an account? <Link to='/signin'> Sign In </Link>
+              </span>
+            </CenterDiv>
+          </CardDiv>
+        </form>
+      </LandingWraper>
+    </>
+  );
+};
+export default Register;

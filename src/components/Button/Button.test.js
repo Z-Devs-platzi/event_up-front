7e1876755f7381eeb03be6ce4d884c9de9fb1ad7
Jorge Ossa <jorgehossa@gmail.com>
2020-08-29 09:06:03 -0500

    Add outline button test

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Button from './index';
-import { create } from 'react-test-renderer';
 
 describe('<Button />', () => {
-  test('Component render of Button', () => {
-    const button = mount(<Button children={'some text'} />);
+  test('Component render of Button Primary', () => {
+    const button = mount(<Button>Some Text</Button>);
+    expect(button.length).toEqual(1);
+  });
+  test('Component render of Button Outline', () => {
+    const button = mount(<Button buttonType='outline'>Some Text</Button>);
     expect(button.length).toEqual(1);
   });
 });

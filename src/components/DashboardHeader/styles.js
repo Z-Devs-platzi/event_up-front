362d507b8ad66d9ae26bc3801d8c493601f2a474
Jorge Ossa <jorgehossa@gmail.com>
2020-08-30 00:46:20 -0500

    Add Dashboard header component

---
+++ src/components/DashboardHeader/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { BiChevronDown } from 'react-icons/bi';
+
+export const HeaderWrapper = styled.header`
+  display: flex;
+  align-items: center;
+  padding: 1.25rem 1rem;
+  background-color: var(--light-green);
+`;
+
+export const HeaderTitle = styled.h3`
+  display: flex;
+  align-items: center;
+  margin-left: 1rem;
+  font-size: 1.25rem;
+  cursor: pointer;
+`;

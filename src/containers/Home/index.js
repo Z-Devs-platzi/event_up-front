e2457b6176aaadcd5a4abdc7b24bd6d4133a8cd6
Alejandro Medina <5almero5@gmail.com>
2020-08-29 20:54:08 -0500

    remove unless data

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,30 +1,13 @@
 import React from 'react';
-import { useFetch } from '../../utility/hooks/useFetch';
-import { SearchPeopleSW } from '../../utility/enpoints/search';
 import { useAuthState } from '../../context/auth-context';
 import { useUser } from '../../context/user-context';
 const Home = () => {
   let { logout } = useAuthState();
   let user = useUser();
-  const { response, isLoading } = useFetch(SearchPeopleSW, {
-    params: { page: '2' },
-    body: null,
-    path: '',
-  });
-  const { data = [] } = response;
   return (
     <>
       <section>
         <h2>HOOME {user.name}</h2>
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : (
-          <ul>
-            {data.map((i) => (
-              <li key={i.id}>{i.name}</li>
-            ))}
-          </ul>
-        )}
         <button onClick={() => logout()}>LogOut</button>
       </section>
     </>

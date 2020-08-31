1e8f9ba6d19b4813dda0acdf3dfa7afeeecd3483
Andres Valencia <cryos74@gmail.com>
2020-08-30 20:22:13 -0500

    Try to fix test

--- src/components/TemplateCountdown/index.js
+++ src/components/TemplateCountdown/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import TimerLabel from '../TimerLabel';
+import PropTypes from 'prop-types';
 import { CountdownSection, Phrase, Timer } from './styles';
 
-const TemplateCountdown = () => {
+const TemplateCountdown = ({ date }) => {
   return (
     <CountdownSection>
       <Phrase>Time until event</Phrase>
       <Timer>
-        <TimerLabel />
+        <TimerLabel date={date} />
       </Timer>
     </CountdownSection>
   );
+};
+
+TemplateCountdown.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+};
+
+const tomorrow = new Date();
+TemplateCountdown.defaultProps = {
+  date: tomorrow.setDate(tomorrow.getDate() + 1),
+  sizeTime: 96,
+  sizeLabel: 23,
+  colorTime: '#FFD43B',
+  colorLabel: '#DBE7E8',
+  showLabel: true,
 };
 
 export default TemplateCountdown;

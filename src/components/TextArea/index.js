44777dcac6e178da76f4373746ec88a4771a3df3
Alejandro Medina <5almero5@gmail.com>
2020-09-09 18:23:35 -0500

    Add logical to text inputs

--- src/components/TextArea/index.js
+++ src/components/TextArea/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { TextareaInput, LabelText, InputContainer, SpanError } from './styles';
 
-const TextArea = ({ label, id, rows, errorMessage }) => {
+const TextArea = ({ label, id, rows, errorMessage, logical }) => {
   return (
     <InputContainer>
       <LabelText htmlFor={id}>{label}</LabelText>
-      <TextareaInput rows={rows}></TextareaInput>
+      <TextareaInput rows={rows} {...logical}></TextareaInput>
       <SpanError>{errorMessage}</SpanError>
     </InputContainer>
   );
@@ -17,13 +17,17 @@ TextArea.propTypes = {
   label: PropTypes.string,
   rows: PropTypes.number,
   errorMessage: PropTypes.string,
+  logical: PropTypes.shape({
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+  }),
 };
 
 TextArea.defaultProps = {
   id: null,
   label: 'Textarea',
   rows: 5,
-  errorMessage: 'Este campo es necesario',
+  errorMessage: '',
 };
 
 export default TextArea;
